14429b41234d3dc5e634507c54a0e0b545c9a050
unknown <aazzouz429@gmail.com>
2020-12-14 00:34:28 +0100

    final commit

---
+++ minjs.js
@@ -0,0 +1,8 @@
+var string = "Saknar ni en engagerad LIA praktikant inom webbutveckling?";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
+
